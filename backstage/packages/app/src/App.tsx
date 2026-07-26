da7932ceb857ff610eb4866bffec5f9a56ca02d1
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2026-07-26 15:17:06 -0400

    fix: register notificationsPlugin and signalsPlugin in frontend app

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -1,7 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import notificationsPlugin from '@backstage/plugin-notifications';
+import signalsPlugin from '@backstage/plugin-signals';
 import { navModule } from './modules/nav';
 
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [catalogPlugin, navModule, notificationsPlugin, signalsPlugin],
 });
